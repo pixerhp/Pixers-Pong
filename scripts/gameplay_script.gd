493f35db73ddfeb8ecaad059fa624ed98eabd5ab
Pixer Pinecone <99830732+pixerhp@users.noreply.github.com>
2026-02-06 21:58:02 -0500

    Create gameplay script + Unique nodes name access

---
+++ scripts/gameplay_script.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+#@onready var left_paddle_noderef: Node2D = %LeftPaddle
+#@onready var right_paddle_noderef: Node2D = %RightPaddle
+#@onready var ball_noderef: Node2D = %Ball
+
+func _process(delta):
+	pass
