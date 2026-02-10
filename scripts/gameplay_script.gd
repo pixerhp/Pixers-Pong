41ea89cb8ed82b42a949d6f301e96eb25b21ae7d
Pixer Pinecone <99830732+pixerhp@users.noreply.github.com>
2026-02-10 01:57:20 -0500

    Implement paddle knockback bump animation

--- scripts/gameplay_script.gd
+++ scripts/gameplay_script.gd
@@ -16,7 +16,7 @@ var PADDLE_SLOWFACTOR: float = 0.05 # (lower numbers = higher friction)
 
 var ball_speedup_amount: float = 50
 
-var ball_velocity: Vector2 = Vector2(-2 * 120, 0.5 * 120)
+var ball_velocity: Vector2 = Vector2(-4 * 120, 0 * 120)
 
 var ball_trail_ms_duration: float = 250
 var ball_trail_positions: PackedVector2Array = []
@@ -28,6 +28,8 @@ func _process(delta: float):
 	handle_paddle_movement(false, delta)
 	handle_ball_movement(delta)
 	handle_ball_trail()
+	handle_paddle_knockback_animation(true)
+	handle_paddle_knockback_animation(false)
 
 func check_do_player_ai():
 	#var test_event = InputEventAction.new()
@@ -84,12 +86,12 @@ func handle_paddle_movement(is_plr1: bool, delta: float):
 	# Limit paddle velocity:
 	pad_vel = clampf(pad_vel, -1 * slow_effect * PADDLE_MAXSPEED, slow_effect * PADDLE_MAXSPEED,)
 	
-	# Move paddle by velocity and limit position:
+	# Move paddle by velocity, limit position, and break velocity when hitting walls:
 	paddle_noderef.position.y = clamp(
 		paddle_noderef.position.y + (pad_vel * delta), PADDLE_UP_LIMIT, PADDLE_DOWN_LIMIT,)
-	if (paddle_noderef.position.y == PADDLE_UP_LIMIT) and (pad_vel < 0.0):
+	if (paddle_noderef.position.y <= PADDLE_UP_LIMIT) and (pad_vel < 0.0):
 		pad_vel = 0.0
-	if (paddle_noderef.position.y == PADDLE_DOWN_LIMIT) and (pad_vel > 0.0):
+	if (paddle_noderef.position.y >= PADDLE_DOWN_LIMIT) and (pad_vel > 0.0):
 		pad_vel = 0.0
 	
 	# Update metadata for next cycle:
@@ -127,10 +129,41 @@ func handle_ball_trail():
 	# Update trail Line2D node's internal array.
 	%BallTrailLine.points = ball_trail_positions
 
+func handle_paddle_knockback_animation(is_plr1: bool):
+	# < 450 is no time, 2000+ is max at ~400ms?
+	const MIN_OOMF: float = 200
+	const MAX_OOMF: float = 2000
+	const MIN_ANIM_LENGTH: int = 50
+	const MAX_ANIM_LENGTH: int = 100
+	const OOMF_LURCH_RATIO: float = 0.005
+	
+	var mesh_noderef: Node2D = (%LeftPaddleMesh if is_plr1 else %RightPaddleMesh)
+	
+	var oomf: float = min(mesh_noderef.get_meta("knockback_oomf"), MAX_OOMF)
+	var start_time: int = mesh_noderef.get_meta("knockback_time")
+	var time_since: int = Time.get_ticks_msec() - start_time
+	if (oomf < MIN_OOMF) or (time_since > MAX_ANIM_LENGTH):
+		mesh_noderef.position.x = 0.0
+		return
+	
+	
+	var anim_duration: int = int(((oomf - MIN_OOMF) / (MAX_OOMF - MIN_OOMF)) * float(MAX_ANIM_LENGTH))
+	if (time_since > anim_duration) or (anim_duration < MIN_ANIM_LENGTH):
+		mesh_noderef.position.x = 0.0
+		return
+	var anim_progress_percent: float = (float(time_since) / float(anim_duration))
+	var anim_weight: float = 1 - pow(((2 * anim_progress_percent) - 1), 2)
+	
+	mesh_noderef.position.x = anim_weight * oomf * OOMF_LURCH_RATIO * (-1.0 if is_plr1 else 1.0)
+	
+	if is_plr1:
+		print(anim_progress_percent)
 
 
 func _on_left_paddle_collider_area_entered(_area):
 	if ball_velocity.x < 0.0:
+		%LeftPaddleMesh.set_meta("knockback_oomf", abs(ball_velocity.x))
+		%LeftPaddleMesh.set_meta("knockback_time", Time.get_ticks_msec())
 		var pad_hit_offset: float = pow((%Ball.position.y - %LeftPaddle.position.y) / (((%LeftPaddleMesh.mesh.height + 5) / 2.0)), 5)
 		var angle: Vector2 = Vector2.RIGHT.rotated(PI * pad_hit_offset * 0.25)
 		ball_velocity = ball_velocity.bounce(angle)
@@ -138,6 +171,8 @@ func _on_left_paddle_collider_area_entered(_area):
 
 func _on_right_paddle_collider_area_entered(_area):
 	if ball_velocity.x > 0.0:
+		%RightPaddleMesh.set_meta("knockback_oomf", abs(ball_velocity.x))
+		%RightPaddleMesh.set_meta("knockback_time", Time.get_ticks_msec())
 		var pad_hit_offset: float = pow((%RightPaddle.position.y - %Ball.position.y) / (((%RightPaddleMesh.mesh.height + 5) / 2.0)), 5)
 		var angle: Vector2 = Vector2.LEFT.rotated(PI * pad_hit_offset * 0.25)
 		ball_velocity = ball_velocity.bounce(angle)
